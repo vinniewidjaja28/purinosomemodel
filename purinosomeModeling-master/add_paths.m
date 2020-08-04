d6936f73b942bd1916ecffe01dbb1db516d2617d
vinniewidjaja28 <vinniewidjaja28@gmail.com>
2020-08-04 15:17:01 -0700

    updated params and added Zaeda's file to repository

---
+++ purinosomeModeling-master/add_paths.m
@@ -0,0 +1,14 @@
+% add paths, must be changed for each file system
+% adding the paths allows you to run different functions in all the
+% folders, without being in those folders.
+
+%vinniewidjaja28 macbook pro
+% main path folder for code
+addpath('/Users/vinniewidjaja28/desktop/matlab/purinosomeModeling-master')
+% numerical code path
+addpath('/Users/vinniewidjaja28/desktop/matlab/purinosomeModeling-master/numericalcode')
+% analytical code path
+addpath('/Users/vinniewidjaja28/desktop/matlab/purinosomeModeling-master/analyticcode')
+% figure code path
+addpath('/Users/vinniewidjaja28/desktop/matlab/purinosomeModeling-master/figurescripts')
+
